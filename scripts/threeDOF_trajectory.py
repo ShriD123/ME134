f33fcae71940fe965dcbc6cad1ece19fbecb7279
Shrikeshav Deshmukh <shrikeshav.d@gmail.com>
2022-01-12 15:21:39 -0800

    Added Imports and Segments
    To be changed... For now, adding the trajectory as a self.segments variable to test sinusoidal output

--- scripts/threeDOF_trajectory.py
+++ scripts/threeDOF_trajectory.py
@@ -8,7 +8,11 @@
 #
 import rospy
 import math
+import kinematics as kin
+import numpy as np
 
+from splines import CubicSpline, Goto, Hold, Stay, QuinticSpline, Goto5
+from kinematics import p_from_T, q_from_T, R_from_T, T_from_Rp, Rx, Ry, Rz
 from sensor_msgs.msg   import JointState
 
 
@@ -23,17 +27,56 @@ class Generator:
         # we don't start sending messages until someone is listening.
         self.pub = rospy.Publisher("/joint_states", JointState)
         rospy.sleep(0.25)
+        
+        # Grab the robot's URDF from the parameter server.
+        robot = Robot.from_parameter_server()
+
+        # Instantiate the Kinematics
+        self.kin = kin.Kinematics(robot, 'world', 'tip')  
+
+        # Initialize the current segment index and starting time t0.
+        q_init = np.array([]).reshape((7,1))
+        self.q_prev = q_init
+        self.index = 0
+        self.t0    = 0.0
+        self.t_prev = 0.0
+        self.lamb = 5
+        
+        self.segments = (Hold(0.0, 1.0, 'Path'),
+                         Goto(0.0, 1.0, 1.5, 'Path'),
+                         Goto(1.0, 2.0, 1.5, 'Path'),
+                         Goto(2.0, 1.0, 1.5, 'Path'),
+                         Goto(1.0, 0.0, 1.5, 'Path'))
 
     # Update every 10ms!
     def update(self, t):
         # Create an empty joint state message.
         cmdmsg = JointState()
+        
+        # If the current segment is done, shift to the next.
+        if (t - self.t0 >= self.segments[self.index].duration()):
+            self.t0    = self.t0 + self.segments[self.index].duration()
+            self.index = (self.index+1)
+            # If the list were cyclic, you could go back to the start with
+            #self.index = (self.index+1) % len(self.segments)
+            #self.last_guess = self.reset_guess 
+            
+        # Check whether we are done with all segments
+        if (self.index >= len(self.segments)):
+            rospy.signal_shutdown("Done with motion")
+            return
 
-        # Use the same names as in the URDF!
-        cmdmsg.name = ['theta1', 'theta2', 'theta3']
-
-        # Set the positions as a function of time.
-        cmdmsg.position = [2*math.sin(2*t), math.cos(t), 0]
+        if (self.segments[self.index].space() == 'Joint'):
+        	# Set the message positions/velocities as a function of time.
+       		(cmdmsg.position, cmdmsg.velocity) = \
+            self.segments[self.index].evaluate(t-self.t0)
+        else:
+        	(cart_position, cart_velocity) = \
+        	self.segments[self.index].evaluate(t-self.t0)
+        	cmdmsg.position = self.kin.ikin(cart_position, self.last_guess)
+        	(T, J) = self.kin.fkin(cmdmsg.position)
+        	cmdmsg.velocity = np.linalg.inv(J[0:3,0:3]) @ cart_velocity
+        	self.last_guess = cmdmsg.position
 
         # Send the command (with the current time).
         cmdmsg.header.stamp = rospy.Time.now()
