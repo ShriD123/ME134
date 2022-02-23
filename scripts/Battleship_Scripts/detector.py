c8ab096a9433de40d02b39a69ca09a32f528f1b3
ggsv-134 <me134team@outlook.com>
2022-02-22 21:12:04 -0800

    Testing Blob Detection
    Playing around with parameters

--- scripts/Battleship_Scripts/detector.py
+++ scripts/Battleship_Scripts/detector.py
@@ -1,9 +1,16 @@
 #!/usr/bin/env python3
 
+from multiprocessing.sharedctypes import Value
 import rospy
 import math
 import kinematics as kin
 import numpy as np
+import cv2
+from cv_bridge import CvBridge
+from sensor_msgs.msg import Image
+from detector_demo.msg import SingleDetection, ManyDetections
+from sensor_msgs.msg import CameraInfo
+from ME134.msg import array
 
 ''' This code contains the functionality of the detector node as well as all the constituent params.'''
 
@@ -35,17 +42,36 @@ class Detector:
     #
     def __init__(self, detection_interval=None):
         rospy.init_node('aruco_detector')
+
+        # Grab an instance of the camera data with predetermined calibration.
+        rospy.loginfo("Waiting for camera info...")
+        msg = rospy.wait_for_message('/usb_cam/camera_info', CameraInfo);
+        self.camD = np.array(msg.D).reshape(5)
+        self.camK = np.array(msg.K).reshape((3,3))
+
+        # Report (to see what is happening).
+        rospy.loginfo("Received Distortion:    \n %s", self.camD)
+        rospy.loginfo("Received Camera Matrix: \n %s", self.camK)
+        rospy.loginfo("Image Center at (%7.2f, %7.2f)",
+                      self.camK[0][2], self.camK[1][2])
+        rospy.loginfo("FOV %6.2f deg horz, %6.2f def vert",
+                      np.rad2deg(np.arctan(msg.width /2/self.camK[0][0])*2),
+                      np.rad2deg(np.arctan(msg.height/2/self.camK[1][1])*2))
+
+        # Arrays that hold the center location and ids of 4 aruco markers
+        self.marker_id = np.zeros(4)
+        self.center_holder = np.zeros(8)
         
         # Prepare the detection publisher and detection message.
         # See Msg/SingleDetection.msg and Msg/ManyDetections.msg for details.
-        self.detections_pub = rospy.Publisher(
-            'detections', ManyDetections, queue_size=10)
+        # self.detections_pub = rospy.Publisher(
+        #     'detections', ManyDetections, queue_size=10)
 
-        # TODO
-        self.images_pub = rospy.Publisher(
-            'detection_images_thresh', Image, queue_size=10)
+        # # TODO
+        # self.images_pub = rospy.Publisher(
+        #     'detection_images_thresh', Image, queue_size=10)
 
-        self.detections_msg = ManyDetections()
+        # self.detections_msg = ManyDetections()
 
         # Prepare OpenCV bridge used to translate ROS Image message to OpenCV
         # image array.
@@ -74,7 +100,7 @@ class Detector:
         image = self.bridge.imgmsg_to_cv2(data, 'bgr8')
         
         # Convert BGR image to HSV.
-        hsv = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
+        #hsv = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
     
     
         # Grab the image.
@@ -98,8 +124,13 @@ class Detector:
         print(ids)
         
         if ids is None:
-            return
+            raise ValueError('No Aruco Markers Detected!')
         
+ 
+
+        i = 0
+        j = 0
+
         # Loop over each marker: the list of corners and ID for this marker.
         for (markercorners, markerid) in zip(corners, ids.flatten()):
 
@@ -119,6 +150,9 @@ class Detector:
             br  = tuple(bottomRight.astype(int))
             ctr = tuple(center.astype(int))             # Center point
             txt = (tl[0], tl[1] - 15)                   # Where to write ID#
+            self.marker_id[j] = markerid
+            self.center_holder[i] = ctr[0]
+            self.center_holder[i+1] = ctr[1]
 
             # Pick the drawing colors.
             boxcolor    = (0, 255, 0)
@@ -139,6 +173,8 @@ class Detector:
                         cv2.FONT_HERSHEY_SIMPLEX, 0.5, textcolor, 2)
 
             print("ArUco marker ID:", markerid)
+            i = i + 2
+            j = j + 1
 
         # Show the output image
         # cv2.imshow("Processed Image", image)
@@ -149,20 +185,116 @@ class Detector:
     
 
     #
-    # Calibrate the Detector
-    #
-    def calibrate(self):
-        #TODO: Implement calibration at some interval (not same as servo rate necessarily)
-        pass
-
-
-    #
     # Determine the XYZ position (in task space) of some detected object
     #
-    def find_xyz(self):
-        #TODO: Implement using point cloud and ArUco later
-        pass
+    def find_xyz(self, data):
+        # Run the Aruco Detect to set up the board
+        self.aruco_detect(data)
 
+        # Close to thrower, far from receiver
+        coord = np.array([[0,1],[2,3],[4,5],[6,7],[8,9]])
+        for i in [0,1,2,3,4]:
+            if self.marker_id[i] == 0:
+                p0 = (0, 0)
+                a0 = (self.center_values[coord[i,0]], self.center_values[coord[i,1]])
+            # Far from both
+            elif self.marker_id[i] == 1:
+                p1 = (0, 0.555)
+                a1 = (self.center_values[coord[i,0]], self.center_values[coord[i,1]])
+            # Close to both
+            elif self.marker_id[i] == 2:
+                p2 = (0.555, 0)
+                a2 = (self.center_values[coord[i,0]], self.center_values[coord[i,1]])
+            # Close to receiver, far from thrower
+            elif self.marker_id[i] == 3:
+                p3 = (0.555, 0.555)
+                a3 = (self.center_values[coord[i,0]], self.center_values[coord[i,1]])
+            elif self.marker_id[i] == 4:
+                test_point = (self.center_values[coord[i,0]], self.center_values[coord[i,1]])
+        
+        pixels = np.float32([[a0],[a1],[a2],[a3]])
+        coords = cv2.undistortPoints(pixels, self.camK, self.camD)
+        points = np.float32([p0, p1, p2, p3])
+        self.M = cv2.getPerspectiveTransform(coords, points)
+
+        # TODO implement blob detection here
+        sack_loc = self.sack_detector(data)
+
+
+        pixels = np.float32([[sack_loc[0],sack_loc[1]]])
+        coords = cv2.undistortPoints(pixels, self.camK, self.camD)
+        points = cv2.perspectiveTransform(coords, self.M)
+        return points[0,0,:]
+
+
+    def sack_detector(self, data):
+        # Check if the detector should be run (has long enough passed since
+        # the last detection?).
+        if self.detection_interval is not None \
+                and (rospy.Time.now() - self.last_detection).to_sec() < self.detection_interval:
+            return
+
+        self.last_detection = rospy.Time.now()
+
+        # Convert ROS Image message to OpenCV image array.
+        img = self.bridge.imgmsg_to_cv2(data, 'bgr8')
+
+        # Convert BGR image to HSV.
+        hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
+
+        # Threshold the HSV image by the max/min Hue, Saturation, Value given.
+        # Be careful with Hue as it wraps around!
+        if (self.upper_lims[0] < self.lower_lims[0]):
+            wrap_upper_lims = (180, self.upper_lims[1], self.upper_lims[2])
+            wrap_lower_lims = (0, self.lower_lims[1], self.lower_lims[2])
+            thresh = cv2.inRange(hsv, self.lower_lims, wrap_upper_lims) + \
+                cv2.inRange(hsv, wrap_lower_lims, self.upper_lims)
+        else:
+            thresh = cv2.inRange(hsv, self.lower_lims, self.upper_lims)
+
+        # Remove noise from the image via erosion/dilation.
+        kernel = np.ones((3, 3), np.uint8)
+        thresh = cv2.erode(thresh, kernel, iterations=5)
+        thresh = cv2.dilate(thresh, kernel, iterations=5)
+        
+        # Publish the image viewed by the camera
+        self.images_pub.publish(self.bridge.cv2_to_imgmsg(thresh, 'mono8'))
+
+        # Find contours within the thresholded image
+        contours, _ = cv2.findContours(
+            thresh, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+
+        # Select only the circular contours
+        # Note, this isn't a great way of doing this, and is taken from here:
+        # https://www.authentise.com/post/detecting-circular-shapes-using-contours
+        # Hough circles are likely better and easier way of detecting circles
+        # specifically (the contour method can easily be adapted to rectangles or
+        # other shapes, however). For an example of Hough circles, see:
+        # https://www.pyimagesearch.com/2014/07/21/detecting-circles-images-using-opencv-hough-circles/
+        circular_contours = []
+        for contour in contours:
+            approx = cv2.approxPolyDP(
+                contour, 0.01*cv2.arcLength(contour, True), True)
+            area = cv2.contourArea(contour)
+            if ((len(approx) > 8) & (area > 30)):
+                circular_contours.append(contour)
+
+        # Construct detection message.
+        self.detections_msg.detections = []
+        for contour in circular_contours:
+            x, y, w, h = cv2.boundingRect(contour)
+            detection_msg = SingleDetection()
+
+            detection_msg.x = x + w / 2
+            detection_msg.y = y + h / 2
+            detection_msg.size_x = w
+            detection_msg.size_y = h
+
+            self.detections_msg.detections.append(detection_msg)
+
+        # Send detection message.
+        self.detections_pub.publish(self.detections_msg)
+        return detection_msg.x, detection_msg.y
 
     #
     # Start the Detector (not sure if we really need this function)
@@ -170,7 +302,7 @@ class Detector:
     def start(self):
         # Start the image subscriber (listen for images from camera node).
         rospy.Subscriber('/usb_cam/image_raw', Image,
-                         self.aruco_detect)
+                         self.find_xyz)
 
         while not rospy.is_shutdown():
             self.servo.sleep()
